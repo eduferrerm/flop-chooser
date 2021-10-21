5ae800fa7c5b794ae5a783e7926e47ae38ba9116
Eduardo E Ferrer M <ekferrerm@gmail.com>
2021-10-21 22:40:48 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FLOP_DATA } from './FLOP_DATA';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'flop-chooser';
+export class AppComponent  {
+
+
+  constructor() {
+
+  }
+
+  // Feel free to put your own data here if you have other favorite flops
+  flops = FLOP_DATA;
+	
+
+
+
 }
